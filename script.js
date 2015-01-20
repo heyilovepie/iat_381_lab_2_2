345a6830c94bf3de223e884ca4266b490c4a90dd
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2015-01-20 14:45:19 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var myapp = angular.module('myapp', []);
+
+myapp.controller('MyController', function ($scope) {
+  $scope.phones = [
+    { name: 'Nexus 5', description: 'Something' },
+    { name: 'Nexus 6', description: 'Something' },
+    { name: 'OnePlus One', description: 'Something' },
+    { name: 'iPhone 6', description: 'Something' }
+  ];
+});

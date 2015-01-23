2bf48d50793d8ed3b1a48b0568f6bb6a9edbd405
Gillian Ramsay <gillianramsay@shaw.ca>
2015-01-23 15:48:29 -0800

    adding changes to content

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var myapp = angular.module('myapp', []);
 
 myapp.controller('MyController', function ($scope) {
-  $scope.phones = [
-    { name: 'Nexus 5', description: 'Something' },
-    { name: 'Nexus 6', description: 'Something' },
-    { name: 'OnePlus One', description: 'Something' },
-    { name: 'iPhone 5', description: 'Something' },
-    { name: 'iPhone 6', description: 'Something' }
+  $scope.sounds = [
+    { name: 'Light', description: 'http://www.freesound.org/people/hanstimm/sounds/9164/' },
+    { name: 'Machine', description: 'http://www.freesound.org/people/harri/sounds/9207/' },
+    { name: 'Glass', description: 'http://www.freesound.org/people/harri/sounds/9209/' },
+    { name: 'Machine', description: 'http://www.freesound.org/people/UATaudio/sounds/31155/' },
+    { name: 'Squeek', description: 'http://www.freesound.org/people/johnnypanic/sounds/34567/' },
+    { name: 'Glass', description: 'http://www.freesound.org/people/juskiddink/sounds/62055/' },
+    { name: 'Machine', description: 'http://www.freesound.org/people/suoitnop/sounds/66262/' },
+    { name: 'Light', description: 'http://www.freesound.org/people/JoelAudio/sounds/77691/' },
+    { name: 'Machine', description: 'http://www.freesound.org/people/Robinhood76/sounds/103635/' },
+    { name: 'Door', description: 'http://www.freesound.org/people/joedeshon/sounds/117416/' }
   ];
-
-  $scope.myCatName = "Splash";
 });

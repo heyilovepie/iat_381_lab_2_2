d1e89b56a05ba023bff2a538afe81a56d1bf2b41
Gillian <heyilovepie@outlook.com>
2015-01-26 11:10:01 -0800

    add search

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-var myapp = angular.module('myapp', []);
+var myapp = angular.module("myapp", []);
 
-myapp.controller('MyController', function ($scope) {
-  $scope.phones = [
-    { name: 'Nexus 5', description: 'Something' },
-    { name: 'Nexus 6', description: 'Something' },
-    { name: 'OnePlus One', description: 'Something' },
-    { name: 'iPhone 5', description: 'Something' },
-    { name: 'iPhone 6', description: 'Something' }
+myapp.controller("MyController", function ($scope) {
+  $scope.text = "klnlknlk lkjlkj kljlkjjjjjj";
+
+  $scope.words = new Array();
+
+  $scope.beers = [
+  new Beer("first beer", "The first brewery", 9.0,
+    ["ale", "pale ale"]),
+  new Beer("second beer", "The second brewery", 8.2,
+    ["ale", "brown ale"]),
+  new Beer("third beer", "The third brewery", 9.5,
+    ["ale", "pale ale", "strong pale ale"]),
+  new Beer("fourth beer", "The fourth brewery", 5.0,
+    ["lagers", "germal lager"])
   ];
 
-  $scope.myCatName = "Splash";
+  function Beer(name, brewery, percentage, categories)
+  {
+    this.name = name;
+    this.brewery = brewery;
+    this.percentage = percentage;
+    this.categories = categories;
+
+    function isInCategory(type){
+    var isTrue = false;
+    for ( var i = 0; i < categories.length; i++){
+        var cat = categories[i];
+        if(cat === type) isTrue = true;
+    }
+    if(type == "") isTrue = true;
+    return isTrue;
+  }
+  }
 });
